00676ac0e967d2a5da567f7d04987e3d707e7341
reskyner <reskyner@googlemail.com>
2020-02-11 15:05:24 +0000

    allow mol block

--- api/utils.py
+++ api/utils.py
@@ -71,7 +71,12 @@ def highlight_diff(prb_mol, ref_mol, width, height):
     if not height:
         height = 200
 
-    mols = [Chem.MolFromSmiles(prb_mol), Chem.MolFromSmiles(ref_mol)]
+    try:
+        ref = Chem.MolFromSmiles(ref_mol)
+    except:
+        ref = Chem.MolFromMolBlock(ref_mol)
+
+    mols = [Chem.MolFromSmiles(prb_mol), ref]
     [Chem.Kekulize(m) for m in mols]
     match = Chem.rdFMCS.FindMCS(mols, ringMatchesRingOnly=True, completeRingsOnly=True)
     match_mol = Chem.MolFromSmarts(match.smartsString)
